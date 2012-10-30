46cb6ceac9ecbc1ff5891e3d5f187e373a6c1aab
Chris Petersen <geek@ex-nerd.com>
2012-10-30 13:57:37 -0700

    adjust benchmark to not test tcp handshake

--- benchmark.py
+++ benchmark.py
@@ -2,16 +2,18 @@
 
 import noeq
 
+client = noeq.Noeq("",'localhost:4444')
+client.connect()
+
 def test(n):
-    c = noeq.Noeq("",'localhost:4444')
-    c.connect()
-    c.get(n)
+    client.get(n)
 
 if __name__ == '__main__':
     import timeit
-    loops = 1000
+    loops = 50000
+    print "Testing with {} loops:".format(loops)
     for n in (1,2,3,5,8,13,21,34,55):
         out = timeit.timeit('test({})'.format(n), setup="from __main__ import test", number=loops)
         perloop = out / loops
         perone = perloop / n
-        print "Gen {}: {:.2f} ms/op {:.2f} ms/id".format(n, perloop * 1000000, perone * 1000000)
+        print "Generate {:>2} ids: {:>10.2f} ms/op {:>10.2f} ms/id {:>10} ids/s".format(n, perloop * 1000000, perone * 1000000, int(round(1 / perone)))
